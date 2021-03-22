220cc5b17d95b08e9cd8cc399eee91721cbddb43
avpimblesr <allan.pimble@gmail.com>
2021-03-21 21:00:38 -0400

    Added context to the two components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import BookList from './BookList'
 import Navbar from './Navbar'
 import '../index.css'
+import ThemeContextProvider from '../contexts/ThemeContext'
 
 const App = () => {
   return (
     <div className='App'>
-      <Navbar />
-      <BookList />
+      <ThemeContextProvider>
+        <Navbar />
+        <BookList />
+      </ThemeContextProvider>
     </div>
   )
 }
